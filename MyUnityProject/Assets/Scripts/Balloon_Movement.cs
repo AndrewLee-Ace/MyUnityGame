a81a923248e8e27922d9e441ed5647d9f60dead2
Andrew Lee <acelee500@gmail.com>
2022-10-26 19:45:52 -0400

    Added balloon script
    Ballon moves across screen and flips and turns direction when reach screen barrier

--- MyUnityProject/Assets/Scripts/Balloon_Movement.cs
+++ MyUnityProject/Assets/Scripts/Balloon_Movement.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Balloon_Movement : MonoBehaviour
 {
+     private float direction = 1;
+     private Vector3 movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement = new Vector3(15 * direction, 0f, 0f);
+        transform.position = transform.position + movement * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        //if (collision.gameObject.tag == "Barrier")
+
+        direction = direction * -1f;
+        transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
     }
 }
